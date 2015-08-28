bed8f5e161ef598038524a909dd489c43b8d3ff4
ijr218 <ijr218@lehigh.edu>
2015-08-28 16:00:22 +0000

    update HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,26 @@
+// Ivan Rivera
+// Lab 01
+// Hellow World Java Program
+//
+//
+//
+
+//define a class
+public class HelloWorld{
+
+    //adding a main method
+    public static void main (String[] args) {
+        //print the statement with a box around it
+        System.out.println("###############################");
+        System.out.println("###############################");
+        System.out.println("#                             #");
+        System.out.println("#     Hello Class of 2015!!   #");
+        System.out.println("#                             #");
+        System.out.println("###############################");
+        System.out.println("###############################");
+        System.out.println("testing");
+        
+        //tests
+    }
+    
+}
