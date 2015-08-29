5d4b4fcc314f5030a321775fab791699dd1110ad
ijr218 <ijr218@lehigh.edu>
2015-08-29 01:09:34 +0000

    complete HW01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+//Ivan Rivera
+//Homework 01
+//Welcome Class Java Program
+//
+//
+//
+//define a class
+public class WelcomeClass{
+    
+    //define a main method
+    public static void main (String[] args){
+        
+        //Print the desired items
+        System.out.println("--------------------");
+        System.out.println("|     WELCOME      |");
+        System.out.println("--------------------");
+        System.out.println(" ^   ^   ^   ^   ^  ");
+        System.out.println("/ \\ / \\ / \\ / \\ / \\ ");
+        System.out.println("<-i--j--r--2--1--8->");
+        System.out.println("\\ / \\ / \\ / \\ / \\ / ");
+        System.out.println(" v   v   v   v   v  ");
+        System.out.println("I actually don't know what a tweet-length statement is.");
+        System.out.println("I just got Facebook this summer. That's the extent of SM.");
+        
+    }
+    
+}
