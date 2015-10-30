ac4cb04f307b5844cb16d43a04c61b4eaf491d81
ijr218 <ijr218@lehigh.edu>
2015-10-30 15:36:39 +0000

    lab10 done AGAIN

---
+++ lab10/Arrays.java
@@ -0,0 +1,27 @@
+/*
+Ivan Rivera
+Prof. Chen
+CSE002
+lab10 Arrays
+*/
+
+/*
+*/
+
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String args[]) {
+        
+        Scanner input = new Scanner(System.in);
+        int size = (int)(Math.random()*5+6);
+        String[] students = new String[size];
+        int[] midterms = new int[size];
+        for(int i=0; i<size; i++) {
+            System.out.print("Student name: ");
+            students[i] = input.next();
+            midterms[i] = (int)(Math.random()*101);
+        }
+        for(int i=0; i<size; i++) {System.out.println(students[i]+": "+midterms[i]);}
+    }
+}
